09e6ad164015addc383d46c262026a4c7801da65
milan-1936 <khpo_bca2077@lict.edu.np>
2025-03-30 20:50:23 +0545

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+class Jadu{
+    static void Main(){
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i<10; i++){
+            builder.Append(i).Append(" ");
+        }
+        Console.WriteLine(builder);
+    }
+}
